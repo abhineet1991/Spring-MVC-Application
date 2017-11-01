bee37c1bc931da865dd7784f383ce2d737f1fdc8
abhineet1991 <32442416+abhineet1991@users.noreply.github.com>
2017-11-01 00:23:11 -0700

    Adding POJOS
    adding pojos for person, visitor and people entities

---
+++ EventCoordinator.java
@@ -0,0 +1,24 @@
+package com.businesstier.entity;
+
+public class EventCoordinator extends People {
+
+	private int eventcoordinatorid;
+	private String places;
+	
+	public int getEventcoordinatorid() {
+		return eventcoordinatorid;
+	}
+
+	public void setEventcoordinatorid(int eventcoordinatorid) {
+		this.eventcoordinatorid = eventcoordinatorid;
+	}
+
+	public String getPlaces() {
+		return places;
+	}
+
+	public void setPlaces(String places) {
+		this.places = places;
+	}
+	
+}
